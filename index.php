7acf827d6a34dc557081f73572ca42b3333c95ac
Lucas Eduardo Pereira da Cruz <lucas.cruz54@fatec.sp.gov.br>
2025-08-13 12:34:19 -0300

    Finalizando classes e colocando ela como Private

--- index.php
+++ index.php
@@ -21,10 +21,12 @@
     var_dump($usuario3);
     echo "</pre>";*/
 
-    echo "Nome:{$usuario1->nome } <br>";
-    echo "Email:{$usuario1->email } <br>";
-    echo "Senha:{$usuario1->senha } <br>";
+    echo "Nome:{$usuario1->getNome() } <br>";
+    echo "Email:{$usuario1->getEmail() } <br>";
+    echo "Senha:{$usuario1->getSenha() } <br>";
 
-    $usuario1->nome = "Maria da Silva";
-    echo "Nome:{$usuario1->nome}<br>";
+    $usuario1->setNome ("Maria da Silva");
+    echo "Nome:{$usuario1->getNome() } <br>";
+
+
 ?>
